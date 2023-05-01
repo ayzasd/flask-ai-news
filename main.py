6e00a5af8b2d2c69fd30c1cff120563112436c71
ayzasd <130077458+ayzasd@users.noreply.github.com>
2023-05-01 08:22:56 -0500

    thing

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-# k
+from flask import Flask, render_template
+import openai
+
+
+# rec
+openai.api_key = API_KEY
+PROMPT = "generate a goofy, funny, fake NY Times style newspaper text."
+app = Flask(__name__)
+
+response = openai.Completion.create(
+    prompt = PROMPT
+) # <-- fix this piece of garbage
+@app.route("/")
+def index():
+    return render_template('index.html') # don't know how to properly acess a chat completion...
